f52c1e383f1fa37f182c263785caaa752359baee
jungwook0103 <0103jungwook@gmail.com>
2025-02-27 15:44:00 +0900

    second

---
+++ innova/src/main/java/com/kim/InnovaApplication.java
@@ -0,0 +1,13 @@
+package com.kim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovaApplication.class, args);
+	}
+
+}
---
+++ innova/src/test/java/com/kim/InnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
